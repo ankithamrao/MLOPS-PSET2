bf94e008984261ddadcebe1cf0b321361a323573
EC2 Default User <ec2-user@ip-172-31-16-255.ec2.internal>
2022-07-06 06:04:33 +0000

    Python setup for training the model and prediction

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,22 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import numpy as np
+import pandas as pd
+import yaml
+from urllib.parse import urlparse
+from sklearn.model_selection import train_test_split
 
+from config import Config
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+Config.DATASET_PATH.mkdir(parents=True, exist_ok=True)
 
+with open ("params.yaml", "r") as fd:
+    params = yaml.safe_load(fd)
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+random_state = params['random_state']
+np.random.seed(random_state)
+df = pd.read_csv(str(Config.ORIGINAL_DATASET_FILE_PATH))
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+#Split the data into training and test sets. (0.75, 0.25) split.
+train, test = train_test_split(df)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+train.to_csv(str(Config.DATASET_PATH / "train.csv"), index=None)
+test.to_csv(str(Config.DATASET_PATH / "test.csv"), index=None)
